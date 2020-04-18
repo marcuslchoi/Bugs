ed34c90ff9c1672b24500aa8502bc7d60a25bd9e
Marcus Choi <marcuslchoi@gmail.com>
2020-04-17 21:26:09 -0700

    add Project struct
    Signed-off-by: Marcus Choi <marcuslchoi@gmail.com>

---
+++ BugTracker/Models/Project.swift
@@ -0,0 +1,16 @@
+//
+//  Project.swift
+//  BugTracker
+//
+//  Created by Marcus Choi on 4/17/20.
+//  Copyright © 2020 Marcus Choi. All rights reserved.
+//
+
+import Foundation
+struct Project
+{
+    let id: String
+    let title: String
+    let modules: [String]
+    let users: [String]
+}
