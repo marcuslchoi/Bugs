9ea4333e12a196f815c70909cdebe89c3618fda9
Marcus Choi <marcuslchoi@gmail.com>
2020-04-29 18:48:54 -0700

    Embedded login vc in navigation controller
    Signed-off-by: Marcus Choi <marcuslchoi@gmail.com>

--- BugTracker/Controllers/ChooseProjectViewController.swift
+++ BugTracker/Controllers/ChooseProjectViewController.swift
@@ -16,6 +16,7 @@ class ChooseProjectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Projects"
         tableView.delegate = self
         tableView.dataSource = self
     }
